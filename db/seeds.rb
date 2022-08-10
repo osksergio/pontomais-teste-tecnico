60835fb6e1a37585b5844da550b89b7582cb2ee2
Sergio Oseko <osk.sergio@gmail.com>
2022-08-09 23:58:49 -0300

    build: populate the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Team.create([{name: "TI"}, {name: "Suporte"}, {name: "Financeiro"} ])
+JobTitle.create([{ name: 'Desenvolvedor back-endimpr' }, { name: 'Desenvolvedor front-end' }, {name: 'Encantador de clientes' } ])
+Employee.create([{first_name: 'Pedro', last_name: 'Alves', cpf: '665.415.876-80', team_id: 2,  job_title_id: 3},
+                 {first_name: 'Tiago', last_name: 'Nogueira', cpf: '032.336.130-75', team_id: 1,  job_title_id: 1},
+                 {first_name: 'João', last_name: 'Neves', cpf: '461.636.517-23', team_id: 1,  job_title_id: 2 }])
